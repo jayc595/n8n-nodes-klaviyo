bbad763b35c10b39df6b32dcc5f6842e16687dda
Jay Vogt <jay@junkbrands.com>
2025-04-22 17:43:49 -0500

    Clean up and reorganize profile config

--- nodes/Klaviyo/Profile.ts
+++ nodes/Klaviyo/Profile.ts
@@ -44,11 +44,115 @@ export const ProfileOperations: INodeProperties[] = [
 				routing: {
 					request: {
 						method: 'PATCH',
+						url: '=/profiles/{{ $parameter.updateProfileId }}'
 					},
 				},
 			},
 		],
 		default: 'getAll',
+	},
+];
+
+const postAttributeFields = [
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'email',
+		value: 'email',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'phone_number',
+		value: 'phone_number',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'external_id',
+		value: 'external_id',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'first_name',
+		value: 'first_name',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'last_name',
+		value: 'last_name',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'organization',
+		value: 'organization',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'locale',
+		value: 'locale',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'title',
+		value: 'title',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'image',
+		value: 'image',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'created',
+		value: 'created',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'updated',
+		value: 'updated',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'last_event_date',
+		value: 'last_event_date',
+	},
+	{
+		name: 'location.address1',
+		value: 'location.address1',
+	},
+	{
+		name: 'location.address2',
+		value: 'location.address2',
+	},
+	{
+		name: 'location.city',
+		value: 'location.city',
+	},
+	{
+		name: 'location.country',
+		value: 'location.country',
+	},
+	{
+		name: 'location.latitude',
+		value: 'location.latitude',
+	},
+	{
+		name: 'location.longitude',
+		value: 'location.longitude',
+	},
+	{
+		name: 'location.region',
+		value: 'location.region',
+	},
+	{
+		name: 'location.zip',
+		value: 'location.zip',
+	},
+	{
+		name: 'location.timezone',
+		value: 'location.timezone',
+	},
+	{
+		name: 'location.ip',
+		value: 'location.ip',
 	},
 ];
 
@@ -64,11 +168,6 @@ const updateProfileFields: INodeProperties[] = [
 				operation: ['update']
 			},
 		},
-		routing: {
-			request: {
-				url: '=/profiles/{{ $value }}',
-			}
-		},
 		required: true
 	},
 	{
@@ -77,7 +176,7 @@ const updateProfileFields: INodeProperties[] = [
 		placeholder: 'Add Attribute',
 		type: 'fixedCollection',
 		default: {},
-		displayOptions: { // the resources and operations to display this element with
+		displayOptions: {
 			show: {
 				resource: ['profile'],
 				operation: ['update']
@@ -95,99 +194,7 @@ const updateProfileFields: INodeProperties[] = [
 						displayName: 'Key',
 						name: 'key',
 						type: 'options',
-						// eslint-disable-next-line n8n-nodes-base/node-param-options-type-unsorted-items
-						options: [
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'email',
-								value: 'email'
-							},
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'phone_number',
-								value: 'phone_number'
-							},
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'external_id',
-								value: 'external_id'
-							},
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'anonymous_id',
-								value: 'anonymous_id'
-							},
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'first_name',
-								value: 'first_name'
-							},
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'last_name',
-								value: 'last_name'
-							},
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'organization',
-								value: 'organization'
-							},
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'locale',
-								value: 'locale'
-							},
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'title',
-								value: 'title'
-							},
-							{
-								// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-								name: 'image',
-								value: 'image'
-							},
-							{
-								name: 'location.address1',
-								value: 'location.address1',
-							},
-							{
-								name: 'location.address2',
-								value: 'location.address2',
-							},
-							{
-								name: 'location.city',
-								value: 'location.city',
-							},
-							{
-								name: 'location.country',
-								value: 'location.country',
-							},
-							{
-								name: 'location.latitude',
-								value: 'location.latitude',
-							},
-							{
-								name: 'location.longitude',
-								value: 'location.longitude',
-							},
-							{
-								name: 'location.region',
-								value: 'location.region',
-							},
-							{
-								name: 'location.zip',
-								value: 'location.zip',
-							},
-							{
-								name: 'location.timezone',
-								value: 'location.timezone',
-							},
-							{
-								name: 'location.ip',
-								value: 'location.ip',
-							},
-						],
+						options: postAttributeFields,
 						default: 'email',
 					},
 					{
@@ -231,15 +238,133 @@ const updateProfileFields: INodeProperties[] = [
 						} }}',
 					},
 				},
-				returnFullResponse: true,
-				ignoreHttpStatusErrors: true,
 			},
 		},
 	},
 ];
 
-const getAllProfileFields: INodeProperties[] = [
+const postProfileFields: INodeProperties[] = [
+	...updateProfileFields,
+]
 
+/* --------------------------------------------------------
+		GET
+----------------------------------------------------------- */
+const getAttributeFields = [
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'email',
+		value: 'email',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'phone_number',
+		value: 'phone_number',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'external_id',
+		value: 'external_id',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'first_name',
+		value: 'first_name',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'last_name',
+		value: 'last_name',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'organization',
+		value: 'organization',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'locale',
+		value: 'locale',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'title',
+		value: 'title',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'image',
+		value: 'image',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'created',
+		value: 'created',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'updated',
+		value: 'updated',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'last_event_date',
+		value: 'last_event_date',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'location',
+		value: 'location',
+	},
+	{
+		name: 'location.address1',
+		value: 'location.address1',
+	},
+	{
+		name: 'location.address2',
+		value: 'location.address2',
+	},
+	{
+		name: 'location.city',
+		value: 'location.city',
+	},
+	{
+		name: 'location.country',
+		value: 'location.country',
+	},
+	{
+		name: 'location.latitude',
+		value: 'location.latitude',
+	},
+	{
+		name: 'location.longitude',
+		value: 'location.longitude',
+	},
+	{
+		name: 'location.region',
+		value: 'location.region',
+	},
+	{
+		name: 'location.zip',
+		value: 'location.zip',
+	},
+	{
+		name: 'location.timezone',
+		value: 'location.timezone',
+	},
+	{
+		name: 'location.ip',
+		value: 'location.ip',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+		name: 'properties',
+		value: 'properties',
+	},
+];
+
+const getAllProfileFields: INodeProperties[] = [
+	// No specific options for getAll
 ];
 
 const getOneProfileFields: INodeProperties[] = [
@@ -288,7 +413,6 @@ const getOneProfileFields: INodeProperties[] = [
 const getProfileFields: INodeProperties[] = [
   ...getAllProfileFields,
   ...getOneProfileFields,
-	...updateProfileFields,
 	{
 		displayName: 'Query Parameters',
 		name: 'queryParameters',
@@ -300,293 +424,7 @@ const getProfileFields: INodeProperties[] = [
 				name: 'profileFields',
 				type: 'multiOptions',
 				hint: 'Select which fields are returned.',
-				// eslint-disable-next-line n8n-nodes-base/node-param-multi-options-type-unsorted-items
-				options: [
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'email',
-						value: 'email',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'phone_number',
-						value: 'phone_number',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'external_id',
-						value: 'external_id',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'first_name',
-						value: 'first_name',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'last_name',
-						value: 'last_name',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'organization',
-						value: 'organization',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'locale',
-						value: 'locale',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'title',
-						value: 'title',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'image',
-						value: 'image',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'created',
-						value: 'created',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'updated',
-						value: 'updated',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'last_event_date',
-						value: 'last_event_date',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'location',
-						value: 'location',
-					},
-					{
-						name: 'location.address1',
-						value: 'location.address1',
-					},
-					{
-						name: 'location.address2',
-						value: 'location.address2',
-					},
-					{
-						name: 'location.city',
-						value: 'location.city',
-					},
-					{
-						name: 'location.country',
-						value: 'location.country',
-					},
-					{
-						name: 'location.latitude',
-						value: 'location.latitude',
-					},
-					{
-						name: 'location.longitude',
-						value: 'location.longitude',
-					},
-					{
-						name: 'location.region',
-						value: 'location.region',
-					},
-					{
-						name: 'location.zip',
-						value: 'location.zip',
-					},
-					{
-						name: 'location.timezone',
-						value: 'location.timezone',
-					},
-					{
-						name: 'location.ip',
-						value: 'location.ip',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'properties',
-						value: 'properties',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'subscriptions',
-						value: 'subscriptions',
-					},
-					{
-						name: 'subscriptions.email',
-						value: 'subscriptions.email',
-					},
-					{
-						name: 'subscriptions.email.marketing',
-						value: 'subscriptions.email.marketing',
-					},
-					{
-						name: 'subscriptions.email.marketing.can_receive_email_marketing',
-						value: 'subscriptions.email.marketing.can_receive_email_marketing',
-					},
-					{
-						name: 'subscriptions.email.marketing.consent',
-						value: 'subscriptions.email.marketing.consent',
-					},
-					{
-						name: 'subscriptions.email.marketing.consent_timestamp',
-						value: 'subscriptions.email.marketing.consent_timestamp',
-					},
-					{
-						name: 'subscriptions.email.marketing.last_updated',
-						value: 'subscriptions.email.marketing.last_updated',
-					},
-					{
-						name: 'subscriptions.email.marketing.method',
-						value: 'subscriptions.email.marketing.method',
-					},
-					{
-						name: 'subscriptions.email.marketing.method_detail',
-						value: 'subscriptions.email.marketing.method_detail',
-					},
-					{
-						name: 'subscriptions.email.marketing.custom_method_detail',
-						value: 'subscriptions.email.marketing.custom_method_detail',
-					},
-					{
-						name: 'subscriptions.email.marketing.double_optin',
-						value: 'subscriptions.email.marketing.double_optin',
-					},
-					{
-						name: 'subscriptions.email.marketing.suppression',
-						value: 'subscriptions.email.marketing.suppression',
-					},
-					{
-						name: 'subscriptions.email.marketing.list_suppressions',
-						value: 'subscriptions.email.marketing.list_suppressions',
-					},
-					{
-						name: 'subscriptions.sms',
-						value: 'subscriptions.sms',
-					},
-					{
-						name: 'subscriptions.sms.marketing',
-						value: 'subscriptions.sms.marketing',
-					},
-					{
-						name: 'subscriptions.sms.marketing.can_receive_sms_marketing',
-						value: 'subscriptions.sms.marketing.can_receive_sms_marketing',
-					},
-					{
-						name: 'subscriptions.sms.marketing.consent',
-						value: 'subscriptions.sms.marketing.consent',
-					},
-					{
-						name: 'subscriptions.sms.marketing.consent_timestamp',
-						value: 'subscriptions.sms.marketing.consent_timestamp',
-					},
-					{
-						name: 'subscriptions.sms.marketing.method',
-						value: 'subscriptions.sms.marketing.method',
-					},
-					{
-						name: 'subscriptions.sms.marketing.method_detail',
-						value: 'subscriptions.sms.marketing.method_detail',
-					},
-					{
-						name: 'subscriptions.sms.marketing.last_updated',
-						value: 'subscriptions.sms.marketing.last_updated',
-					},
-					{
-						name: 'subscriptions.sms.transactional',
-						value: 'subscriptions.sms.transactional',
-					},
-					{
-						name: 'subscriptions.sms.transactional.can_receive_sms_transactional',
-						value: 'subscriptions.sms.transactional.can_receive_sms_transactional',
-					},
-					{
-						name: 'subscriptions.sms.transactional.consent',
-						value: 'subscriptions.sms.transactional.consent',
-					},
-					{
-						name: 'subscriptions.sms.transactional.consent_timestamp',
-						value: 'subscriptions.sms.transactional.consent_timestamp',
-					},
-					{
-						name: 'subscriptions.sms.transactional.method',
-						value: 'subscriptions.sms.transactional.method',
-					},
-					{
-						name: 'subscriptions.sms.transactional.method_detail',
-						value: 'subscriptions.sms.transactional.method_detail',
-					},
-					{
-						name: 'subscriptions.sms.transactional.last_updated',
-						value: 'subscriptions.sms.transactional.last_updated',
-					},
-					{
-						name: 'subscriptions.mobile_push',
-						value: 'subscriptions.mobile_push',
-					},
-					{
-						name: 'subscriptions.mobile_push.marketing',
-						value: 'subscriptions.mobile_push.marketing',
-					},
-					{
-						name: 'subscriptions.mobile_push.marketing.can_receive_push_marketing',
-						value: 'subscriptions.mobile_push.marketing.can_receive_push_marketing',
-					},
-					{
-						name: 'subscriptions.mobile_push.marketing.consent',
-						value: 'subscriptions.mobile_push.marketing.consent',
-					},
-					{
-						name: 'subscriptions.mobile_push.marketing.consent_timestamp',
-						value: 'subscriptions.mobile_push.marketing.consent_timestamp',
-					},
-					{
-						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
-						name: 'predictive_analytics',
-						value: 'predictive_analytics',
-					},
-					{
-						name: 'predictive_analytics.historic_clv',
-						value: 'predictive_analytics.historic_clv',
-					},
-					{
-						name: 'predictive_analytics.predicted_clv',
-						value: 'predictive_analytics.predicted_clv',
-					},
-					{
-						name: 'predictive_analytics.total_clv',
-						value: 'predictive_analytics.total_clv',
-					},
-					{
-						name: 'predictive_analytics.historic_number_of_orders',
-						value: 'predictive_analytics.historic_number_of_orders',
-					},
-					{
-						name: 'predictive_analytics.predicted_number_of_orders',
-						value: 'predictive_analytics.predicted_number_of_orders',
-					},
-					{
-						name: 'predictive_analytics.average_days_between_orders',
-						value: 'predictive_analytics.average_days_between_orders',
-					},
-					{
-						name: 'predictive_analytics.average_order_value',
-						value: 'predictive_analytics.average_order_value',
-					},
-					{
-						name: 'predictive_analytics.churn_probability',
-						value: 'predictive_analytics.churn_probability',
-					},
-					{
-						name: 'predictive_analytics.expected_date_of_next_order',
-						value: 'predictive_analytics.expected_date_of_next_order',
-					}
-				],
+				options: getAttributeFields,
 				routing: {
 					request: {
 						qs: {
@@ -608,5 +446,6 @@ const getProfileFields: INodeProperties[] = [
 ];
 
 export const ProfileFields: INodeProperties[] = [
+	...postProfileFields,
   ...getProfileFields
 ];
